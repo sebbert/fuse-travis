75665af9238ed7b2d680079e0b4c4087879eafe9
Bjørn-Olav Strand <bo@startsiden.no>
2016-04-14 16:17:06 +0200

    Remove after run

--- .travis/install-ndk.sh
+++ .travis/install-ndk.sh
@@ -2,7 +2,8 @@
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 	wget http://dl.google.com/android/repository/android-ndk-r11c-darwin-x86_64.zip
-	unzip android-ndk-r11c-darwin-x86_64.zip | grep -v inflating: | grep -v creating:
+	unzip android-ndk-r11c-darwin-x86_64.zip | grep -v inflating: | grep -v creating: | grep -v extracting:
+	rm android-ndk-r11c-darwin-x86_64.zip
 	export ANDROID_NDK=${PWD}/android-ndk-r11c
 	export ANDROID_NDK_HOME=${PWD}/android-ndk-r11c
 	export PATH=${PATH}:${ANDROID_NDK_HOME}
