7b559d3e56f9c71a10e1e3d3c9ed5505602470e7
Bjørn-Olav Strand <bo@startsiden.no>
2016-04-14 14:14:50 +0200

    Removing iOS

--- .travis/test.sh
+++ .travis/test.sh
@@ -7,12 +7,12 @@ if [ $exitcode -ne 0 ]; then
 	echo Failed uno $name : $exitcode
 	exit $exitcode
 fi
-uno build -tiOS TravisTest.unoproj -v
-exitcode=$?
-if [ $exitcode -ne 0 ]; then
-	echo Failed iOS $name : $exitcode
-	exit $exitcode
-fi
+#uno build -tiOS TravisTest.unoproj -v
+#exitcode=$?
+#if [ $exitcode -ne 0 ]; then
+#	echo Failed iOS $name : $exitcode
+#	exit $exitcode
+#fi
 uno build -tAndroid TravisTest.unoproj -v
 exitcode=$?
 if [ $exitcode -ne 0 ]; then
