03e19d21ab0686a88c42977f3451a5452f8305a9
Bjørn-Olav Strand <bo@startsiden.no>
2016-04-14 16:22:04 +0200

    Some secret chars?

--- .travis/install-ndk.sh
+++ .travis/install-ndk.sh
@@ -3,7 +3,7 @@
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 	echo $NDK_VERSION
 	wget http://dl.google.com/android/repository/android-ndk-r11c-darwin-x86_64.zip
-	unzip android-ndk-r11c-darwin-x86_64.zip | grep -v inflating: | grep -v creating: | grep -v extracting:
+	unzip android-ndk-r11c-darwin-x86_64.zip | grep -v inflating: | grep -v creating: | grep -v extracting:
 	rm android-ndk-r11c-darwin-x86_64.zip
 	export ANDROID_NDK=${PWD}/android-ndk-r11c
 	export ANDROID_NDK_HOME=${PWD}/android-ndk-r11c
