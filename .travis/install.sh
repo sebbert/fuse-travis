abe1703042908400c1ccda6b589ac04d7510af49
Bjørn-Olav Strand <bo@startsiden.no>
2016-04-14 14:06:57 +0200

    Added some caching

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,10 +4,15 @@
 # https://docs.travis-ci.com/user/osx-ci-environment/
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+	ls -l /Applications
+	ls -l /Applications/Fuse.app
+	ls -l fuse
+	mkdir fuse
 	wget https://api.fusetools.com/fuse-release-management/releases/0.11.1.6272/osx
-	mv osx fuse_osx_0_11_1_6262.pkg
-	sudo installer -pkg ./fuse_osx_0_11_1_6262.pkg -target /
+	mv osx fuse/fuse_osx_0_11_1_6262.pkg
+	sudo installer -pkg fuse/fuse_osx_0_11_1_6262.pkg -target /
 	echo "Installed Fuse"
 	ls -l /Applications
 	ls -l /Applications/Fuse.app
+	ls -l fuse
 fi
