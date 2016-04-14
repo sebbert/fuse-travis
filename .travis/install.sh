28e65d16d2b4c4429d052de7b1b9d30a5501f2c0
Bjørn-Olav Strand <bo@startsiden.no>
2016-04-14 13:39:40 +0200

    added some debug

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,6 +1,12 @@
-if [ "`uname`" == "Darwin" ];
-then
+#!/bin/sh
+
+# https://docs.travis-ci.com/user/multi-os/
+
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 	wget https://api.fusetools.com/fuse-release-management/releases/0.11.1.6272/osx
 	mv osx fuse_osx_0_11_1_6262.pkg
 	sudo installer -pkg ./fuse_osx_0_11_1_6262.pkg -target /
+	echo "Installed Fuse"
+	ls -l /Applications
+	ls -l /Applications/Fuse.app
 fi
