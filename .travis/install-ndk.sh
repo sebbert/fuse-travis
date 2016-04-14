793e7ec54cf0fba7945d439e55bd84b58bf2171f
Bjørn-Olav Strand <bo@startsiden.no>
2016-04-14 16:14:23 +0200

    Better output

--- .travis/install-ndk.sh
+++ .travis/install-ndk.sh
@@ -2,7 +2,7 @@
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 	wget http://dl.google.com/android/repository/android-ndk-r11c-darwin-x86_64.zip
-	unzip android-ndk-r11c-darwin-x86_64.zip
+	unzip android-ndk-r11c-darwin-x86_64.zip | grep -v inflating: | grep -v creating:
 	export ANDROID_NDK=${PWD}/android-ndk-r11c
 	export ANDROID_NDK_HOME=${PWD}/android-ndk-r11c
 	export PATH=${PATH}:${ANDROID_NDK_HOME}
