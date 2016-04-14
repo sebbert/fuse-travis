11f9fce4c5d90c3cde7bfd09bc80b64126300264
Bjørn-Olav Strand <bo@startsiden.no>
2016-04-14 14:03:03 +0200

    Adding verbose flag for build

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
 name=TravisTest
-uno build TravisTest.unoproj
+uno build TravisTest.unoproj -v
 exitcode=$?
 if [ $exitcode -ne 0 ]; then
 	echo Failed uno $name : $exitcode
 	exit $exitcode
 fi
-uno build -tiOS TravisTest.unoproj
+uno build -tiOS TravisTest.unoproj -v
 exitcode=$?
 if [ $exitcode -ne 0 ]; then
 	echo Failed iOS $name : $exitcode
 	exit $exitcode
 fi
-uno build -tAndroid TravisTest.unoproj
+uno build -tAndroid TravisTest.unoproj -v
 exitcode=$?
 if [ $exitcode -ne 0 ]; then
 	echo Failed Android $name : $exitcode
