7a70cec6438975411c5d408ae84c9e9a683be611
Bjørn-Olav Strand <bo@startsiden.no>
2016-04-14 15:20:54 +0200

    Added preprogrammed answers

--- .travis/install.sh
+++ .travis/install.sh
@@ -9,15 +9,14 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 	ls -l /Applications
 	ls -l /Applications/Fuse.app
 	ls -l fuse
+	pwd
 	mkdir fuse
 	wget https://api.fusetools.com/fuse-release-management/releases/0.11.1.6272/osx
 	mv osx fuse/fuse_osx_0_11_1_6262.pkg
 	sudo installer -pkg fuse/fuse_osx_0_11_1_6262.pkg -target /
 	sudo chown -R travis $HOME/.fuse
 	echo "Installed Fuse"
-	ls -la /Users/travis
-	ls -la /Users/travis/.fuse
-	echo | fuse install android
+	fuse install android < ./.travis/sdkinstall.txt
 	ls -l /Applications
 	ls -l /Applications/Fuse.app
 	ls -l fuse
