78f30dffe58fb44e7f99a22e3036c7aa4e046cbb
Bjørn-Olav Strand <bo@startsiden.no>
2016-04-14 16:23:18 +0200

    Changed version

--- .travis/install-ndk.sh
+++ .travis/install-ndk.sh
@@ -2,10 +2,10 @@
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 	echo $NDK_VERSION
-	wget http://dl.google.com/android/repository/android-ndk-r11c-darwin-x86_64.zip
-	unzip android-ndk-r11c-darwin-x86_64.zip | grep -v inflating: | grep -v creating: | grep -v extracting:
-	rm android-ndk-r11c-darwin-x86_64.zip
-	export ANDROID_NDK=${PWD}/android-ndk-r11c
-	export ANDROID_NDK_HOME=${PWD}/android-ndk-r11c
+	wget http://dl.google.com/android/repository/android-ndk-${NDK_VERSION}-darwin-x86_64.zip
+	unzip android-ndk-${NDK_VERSION}-darwin-x86_64.zip | grep -v inflating: | grep -v creating: | grep -v extracting:
+	rm android-ndk-${NDK_VERSION}-darwin-x86_64.zip
+	export ANDROID_NDK=${PWD}/android-ndk-${NDK_VERSION}
+	export ANDROID_NDK_HOME=${PWD}/android-ndk-${NDK_VERSION}
 	export PATH=${PATH}:${ANDROID_NDK_HOME}
 fi
