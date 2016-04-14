13e3e2bbede5af902a4d3c9672e43b3eda918b00
Bjørn-Olav Strand <bo@startsiden.no>
2016-04-14 14:55:01 +0200

    fix chown

--- .travis/install.sh
+++ .travis/install.sh
@@ -13,6 +13,7 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 	wget https://api.fusetools.com/fuse-release-management/releases/0.11.1.6272/osx
 	mv osx fuse/fuse_osx_0_11_1_6262.pkg
 	sudo installer -pkg fuse/fuse_osx_0_11_1_6262.pkg -target /
+	sudo chown travis .fuse
 	echo "Installed Fuse"
 	ls -la /Users/travis
 	ls -la /Users/travis/.fuse
