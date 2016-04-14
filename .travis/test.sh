754039e2548af6446543ced81ed3b730e89d6000
Bjørn-Olav Strand <bo@startsiden.no>
2016-04-14 15:35:25 +0200

    Adding better output

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,21 +1,29 @@
 #!/bin/sh
 
 name=TravisTest
+echo travis_fold:start:#uno
 uno build TravisTest.unoproj -v
 exitcode=$?
+echo travis_fold:end:#uno
 if [ $exitcode -ne 0 ]; then
-	echo Failed uno $name : $exitcode
-	exit $exitcode
+	echo "uno $name - FAIL"
+else
+	echo "uno $name - PASS"
 fi
+echo travis_fold:start:#ios
 #uno build -tiOS TravisTest.unoproj -v
 #exitcode=$?
 #if [ $exitcode -ne 0 ]; then
 #	echo Failed iOS $name : $exitcode
 #	exit $exitcode
 #fi
+echo travis_fold:end:#ios
+echo travis_fold:start:#android
 uno build -tAndroid TravisTest.unoproj -v
 exitcode=$?
+echo travis_fold:end:#android
 if [ $exitcode -ne 0 ]; then
-	echo Failed Android $name : $exitcode
-	exit $exitcode
+	echo "uno android $name - FAIL"
+else
+	echo "uno android $name - PASS"
 fi
