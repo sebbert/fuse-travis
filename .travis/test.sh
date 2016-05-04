dbea0e5553a84d047339c9a657b250f5f94d20bd
Bjørn-Olav Strand <bo@startsiden.no>
2016-05-04 16:02:00 +0200

    Trying to fix fold

--- .travis/test.sh
+++ .travis/test.sh
@@ -2,24 +2,24 @@
 
 name=TravisTest
 status=0
-echo [0Ktravis_fold:start:#uno
+echo travis_fold:start:uno
 uno build TravisTest.unoproj -v
 exitcode=$?
-echo [0Ktravis_fold:end:#uno
+echo travis_fold:end:uno
 if [ $exitcode -ne 0 ]; then
 	echo "uno $name - FAIL"
 	status=$exitcode
 else
 	echo "uno $name - PASS"
 fi
-echo [0Ktravis_fold:start:#ios
+echo travis_fold:start:ios
 #uno build -tiOS TravisTest.unoproj -v
 #exitcode=$?
-echo [0Ktravis_fold:end:#ios
-echo [0Ktravis_fold:start:#android
+echo travis_fold:end:ios
+echo travis_fold:start:android
 uno build -tAndroid TravisTest.unoproj -v
 exitcode=$?
-echo [0Ktravis_fold:end:#android
+echo travis_fold:end:android
 if [ $exitcode -ne 0 ]; then
 	echo "uno android $name - FAIL"
 	status=$exitcode
