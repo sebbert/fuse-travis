964cb259121dcb8b240e79a65104e77e81983dc1
Bjørn-Olav Strand <bo@startsiden.no>
2016-05-06 10:18:01 +0200

    Adding back multilang

--- .travis/install-ndk.sh
+++ .travis/install-ndk.sh
@@ -2,7 +2,6 @@
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 	echo "Installing Android NDK version ${NDK_VERSION}"
-	find /usr/local/android-sdk
 	wget http://dl.google.com/android/repository/android-ndk-${NDK_VERSION}-darwin-x86_64.zip
 	unzip android-ndk-${NDK_VERSION}-darwin-x86_64.zip | grep -v inflating: | grep -v creating: | grep -v extracting:
 	rm android-ndk-${NDK_VERSION}-darwin-x86_64.zip
